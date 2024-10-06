3469eeaa3df0508bff1331993c6b9341adc6639f
sihyunjojo <si4018@naver.com>
2024-10-07 00:30:40 +0900

    feat: 초기 설정 파일 추가

---
+++ config/src/test/java/com/joyride/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joyride.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
