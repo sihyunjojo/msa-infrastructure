2b0ac6bb77aaa7c814825e6a3da2fcf4e6a54b37
조시현 <si4018@naver.com>
2024-10-10 13:44:19 +0900

    refactor: security 설정 삭제

--- gateway/src/main/java/com/joyride/gateway/SecurityConfig.java
+++ gateway/src/main/java/com/joyride/gateway/SecurityConfig.java
@@ -1,49 +1,49 @@
-package com.joyride.gateway;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-@Configuration
-@EnableWebFluxSecurity
-public class SecurityConfig {
-
-    @Value("${admin.name}")
-    private String adminName;
-
-    @Value("${admin.password}")
-    private String adminPassword;
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(exchanges -> exchanges
-                        .anyExchange().authenticated()
-                )
-                .httpBasic(httpBasic -> {});
-        return http.build();
-    }
-
-    @Bean
-    public MapReactiveUserDetailsService userDetailsService() {
-        UserDetails user = User
-                .withUsername(adminName)
-                .password(passwordEncoder().encode(adminPassword))
-                .roles("ADMIN")
-                .build();
-        return new MapReactiveUserDetailsService(user);
-    }
-}
+//package com.joyride.gateway;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//
+//@Configuration
+//@EnableWebFluxSecurity
+//public class SecurityConfig {
+//
+//    @Value("${admin.name}")
+//    private String adminName;
+//
+//    @Value("${admin.password}")
+//    private String adminPassword;
+//
+//    @Bean
+//    public BCryptPasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+//        http
+//                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+//                .authorizeExchange(exchanges -> exchanges
+//                        .anyExchange().authenticated()
+//                )
+//                .httpBasic(httpBasic -> {});
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public MapReactiveUserDetailsService userDetailsService() {
+//        UserDetails user = User
+//                .withUsername(adminName)
+//                .password(passwordEncoder().encode(adminPassword))
+//                .roles("ADMIN")
+//                .build();
+//        return new MapReactiveUserDetailsService(user);
+//    }
+//}
